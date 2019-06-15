9fa3ce61eeca1d544c7ac11d978b3b35811c66ae
Ricardo Ianelli <ricardoianelli@hotmail.com>
2019-06-15 15:07:02 -0300

    First implementation of Employee

---
+++ Data1/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace Data1.Entities
+{
+    class Employee
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public double salary { get; set; }
+
+        public Employee()
+        { }
+
+        public Employee(string name, string email, double salary)
+        {
+            this.name = name;
+            this.email = email;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return name + ", " + email + ", " + salary;
+        }
+    }   
+}
---
+++ Data1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using Data1.Entities;
+
+namespace Data1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                List<Employee> employees = new List<Employee>();
+                Console.Write("Enter the csv file path: ");
+                string inPath = Console.ReadLine();
+
+                using (StreamReader reader = File.OpenText(inPath)){
+                    while (!reader.EndOfStream){
+                        string[] line = reader.ReadLine().Split(",");
+                        employees.Add(
+                            new Employee {
+                                name = line[0],
+                                email = line[1],
+                                salary = Double.Parse(line[2], CultureInfo.InvariantCulture)
+                            }
+                        );
+                    }
+                }
+
+                employees.ForEach(p => Console.WriteLine(p));
+            }
+            catch (Exception exception){
+                Console.WriteLine("Error: " + exception.Message);
+            }
+        }
+    }
+}
